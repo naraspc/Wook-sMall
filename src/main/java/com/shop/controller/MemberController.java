d77331258656ea8cd58d1bfaf73201a4e8308469
naraspc <wodnrtkfka@naver.com>
2023-10-16 10:04:27 +0900

    t

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -49,13 +49,13 @@ public class MemberController {
 
     @GetMapping(value = "/login")
     public String loginMember(){
-        return "templates/member/memberLoginForm";
+        return "/member/memberLoginForm";
     }
 
     @GetMapping(value = "/login/error")
     public String loginError(Model model){
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-        return "templates/member/memberLoginForm";
+        return "/member/memberLoginForm";
     }
 
 }
